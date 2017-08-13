9b848351a2f109d649ce525837322921fb40b2a9
Bailey Miller <bmiller@susquehannasoftware.com>
2017-08-13 01:50:22 -0400

    Hello World program

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChangeTitle("My first application in C#");
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+
+        static void ChangeTitle(string title)
+        {
+            Console.Title = title;
+        }
+    }
+}
