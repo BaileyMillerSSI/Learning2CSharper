688343b9c0fa439abe5a1ffa1ebefdccfc4515fc
Bailey Miller <bmiller@susquehannasoftware.com>
2017-08-13 05:51:56 -0400

    Added a fun you tube video uploader.

---
+++ YoutubeVideoUploader/YoutubeVideoUploader/YoutubeVideoUploader.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System.Threading;
+using System.Reflection;
+
+namespace YoutubeVideoUploader
+{
+    public class UploadVideo
+    {
+        string FilePath { get; }
+
+        private double _FileSize
+        {
+            get;
+            set;
+        }
+
+        double FileSize {
+            get
+            {
+                
+                if (_FileSize == 0)
+                {
+                    _FileSize = new FileInfo(FilePath).Length;
+                }
+
+                return _FileSize;
+            }
+        }
+
+        string FileName
+        {
+            get
+            {
+                return Path.GetFileNameWithoutExtension(FilePath);
+            }
+        }
+        public UploadVideo(string path = "")
+        {
+            this.FilePath = String.IsNullOrEmpty(path) ? @"C:\Users\Bailey Miller\Documents\Overwatch\videos\overwatch\junky_kill.mp4" : path;
+            this.BeginUpload();
+        }
+        void BeginUpload()
+        {
+            Console.WriteLine("YouTube Data API: Upload Video");
+            Console.WriteLine("==============================");
+
+            try
+            {
+                Run().Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private async Task Run()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    // This OAuth 2.0 access scope allows an application to upload files to the
+                    // authenticated user's YouTube channel, but doesn't allow other types of access.
+                    new[] { YouTubeService.Scope.YoutubeUpload },
+                    "user",
+                    CancellationToken.None
+                );
+            }
+
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = Assembly.GetExecutingAssembly().GetName().Name
+            });
+
+            var video = new Video();
+            video.Snippet = new VideoSnippet();
+            video.Snippet.Title = FileName;
+            video.Snippet.Description = "Default Video Description";
+            video.Snippet.Tags = new string[] { "tag1", "tag2" };
+            video.Snippet.CategoryId = "22"; // See https://developers.google.com/youtube/v3/docs/videoCategories/list
+            video.Status = new VideoStatus();
+            video.Status.PrivacyStatus = "unlisted"; // or "private" or "public"
+            var filePath = FilePath; // Replace with path to actual movie file.
+
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var videosInsertRequest = youtubeService.Videos.Insert(video, "snippet,status", fileStream, "video/*");
+                videosInsertRequest.ProgressChanged += videosInsertRequest_ProgressChanged;
+                videosInsertRequest.ResponseReceived += videosInsertRequest_ResponseReceived;
+
+                await videosInsertRequest.UploadAsync();
+            }
+        }
+
+        void videosInsertRequest_ProgressChanged(IUploadProgress progress)
+        {
+            switch (progress.Status)
+            {
+                case UploadStatus.Uploading:
+                    Console.WriteLine(String.Format("{0}% Uploaded", Math.Round((progress.BytesSent / FileSize) * 100)));
+                    break;
+
+                case UploadStatus.Failed:
+                    Console.WriteLine("An error prevented the upload from completing.\n{0}", progress.Exception);
+                    break;
+            }
+        }
+
+        void videosInsertRequest_ResponseReceived(Video video)
+        {
+            Console.WriteLine("Video id '{0}' was successfully uploaded.", video.Id);
+        }
+    }
+}
